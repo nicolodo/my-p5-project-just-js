1e350ff6f408bc52063ee928df1ad728acec47c6
nicolas <nicolasbernard19@gmail.com>
2026-02-24 12:08:29 +0000

    create new sketch file

---
+++ sketches/1 gettingStarted.js
@@ -0,0 +1,32 @@
+
+export function gettingStarted1(){
+    // sky blue background
+    background(135,206,235);
+    // sun in top-right corner
+    fill("yellow")
+    stroke("orange") // make outline orange
+    strokeWeight(20) // make outline 20px
+    circle(550, 50,100);
+
+    // Draw grass
+    stroke(0) //outline
+    strokeWeight(1) //outline 1px
+    fill("green") //colour green
+    rect(0, 200,600,200) //from (0,200)xy draw a 600 by 200 block
+    
+    ellipse(100, 100, 50, 50)
+    square(100, 100, 100)
+    triangle(100,100,150,150,50,150)
+    line(400,400,300,300)
+    fill("blue")
+    arc(200, 200, 40, 400, 0, 1.6*PI)
+
+    // emojis
+    textSize(100)
+    text("bottom text", sin(mouseX)*mouseX, sin(mouseY)*mouseY)
+    textSize(25)
+    text("🌠", mouseY, mouseX)
+    textSize(80)
+    text("🌠", 350, 200)
+
+}
