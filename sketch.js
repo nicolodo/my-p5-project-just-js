ad776e4cc33f30a72c577e1d0378f72ddb854674
nicolas <nicolasbernard19@gmail.com>
2026-02-24 14:46:04 +0000

    add variables and Changes into sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
+
+
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(400, 400);
 }
 
 function draw() {
-    gettingStarted1()
+    // gettingStarted1()
     // variablesAndChange()
 }
 
@@ -40,5 +42,42 @@ function gettingStarted1(){
 }
 
 function variablesAndChange(){
+    background(220);
+    //Place code here
     
+    /* places the x a y position of the mouse
+    on the canvas as a coordinate pair x, y */
+    fill(0)
+    text(`${mouseX}, ${mouseY}`, 20, 20);
+}
+
+function mountainsAtNight(){
+    let skyColour = "#200d83"
+    background(skyColour) //black?
+
+    // moon 
+    fill("#fff")
+    circle(width*(7/8), height*(1/8), width*(2/8))
+    stroke(skyColour)
+    fill(skyColour)
+    circle(width*(6.5/8), height*(1/8), width*(2/8))
+
+    // mountains
+    stroke("#000")
+    fill("#3e496a")
+    triangle(width*(-1/8), height*(6/8), width*(4/8), height*(6/8), width*(1/8), height*(2/8))
+    let Mtn2Xshift = 3 // 3 eights Mountain
+    triangle(width*((-1+Mtn2Xshift)/8), height*(6/8), width*((4+Mtn2Xshift)/8), height*(6/8), width*((1+Mtn2Xshift)/8), height*(2/8))
+
+    // grass
+    fill("rgb(28, 88, 31)")
+    rect(width*(0/8),height*(6/8), width, height)
+
+    // displays the x and y position of the mouse on the canvas
+    fill(255) //white
+    text(`mouseX: ${mouseX}, mouseY: ${mouseY}`,20,20);
+
+    // cloud
+    fill(255);
+    ellipse(50,50,80,40)
 }
