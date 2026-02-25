f6438e191ccf7ba56499a7f44eb3c34e7f7f4c17
nicolodo <nicolasbernard19@gmail.com>
2026-02-25 16:15:42 +0000

    create conditionals 2

---
+++ sketches/p5Tutorials/Conditionals2.js
@@ -0,0 +1,15 @@
+
+// sun y coordinate
+let sunHeight = 600;
+
+// variables for color change
+let redVal = 0;
+let greenVal = 0;
+
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw() {
+
+}
